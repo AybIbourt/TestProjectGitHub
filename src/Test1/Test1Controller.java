ce7c84b96f52e13c2d0c6275537e4fd1d0e60184
Hermes Vandevyvere <hermes.vandevyvere@student.vives.be>
2020-02-05 12:25:47 +0100

    Initial commit

---
+++ src/Test1/Test1Controller.java
@@ -0,0 +1,10 @@
+package Test1;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Test1Controller {
+    public TextField txtGetal1;
+    public TextField txtGetal2;
+    public Label lblOPL;
+}
---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module TestProjectGitHub {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.desktop;
+
+    opens Test1;
+}
