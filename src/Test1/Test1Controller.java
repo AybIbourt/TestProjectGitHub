2d5f89ef1701eb80e22ba1252b14ef72644a40a9
Ayoub <r0699832@student.vives.be>
2020-02-05 13:40:28 +0100

    plus button

--- src/Test1/Test1Controller.java
+++ src/Test1/Test1Controller.java
@@ -1,10 +1,21 @@
 package Test1;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class Test1Controller {
     public TextField txtGetal1;
     public TextField txtGetal2;
     public Label lblOPL;
+    public Button btnOptellen;
+
+    public void Clicked_Optellen(MouseEvent mouseEvent) {
+        int Getal1 = Integer.parseInt(txtGetal1.getText());
+        int Getal2 = Integer.parseInt(txtGetal2.getText());
+
+        int oplossing = Getal1 +Getal2;
+        lblOPL.setText(String.valueOf(oplossing));
+    }
 }
